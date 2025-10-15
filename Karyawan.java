918af152307a3407124f0bd1258025078a910e01
Malanarn <malanairin73@gmail.com>
2025-10-15 15:24:27 +0700

    Create Karyawan.java

---
+++ Karyawan.java
@@ -0,0 +1,38 @@
+public class Karyawan {
+    private String nik;
+    private String nama;
+    private String jabatan;
+    private String jabatanBaru;
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public String getJabatanBaru() {
+        return jabatanBaru;
+    }
+
+    public void setJabatanBaru(String jabatanBaru) {
+        this.jabatanBaru = jabatanBaru;
+    }
+}
